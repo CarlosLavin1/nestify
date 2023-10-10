64a5614d6814f0d1c1c0acc0f7030f72ed8ba9b4
Catie <cayteecorn@gmail.com>
2023-10-10 18:01:45 -0300

    began creation of project, started implementing Agent and Staff models - project still missing required EFCore files for proper database and website functionality

---
+++ mvcNestify/mvcNestify/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace mvcNestify.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
