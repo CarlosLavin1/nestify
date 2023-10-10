4654d3da2ed9bf300852eb74052acbf57fb1f05c
Catie <cayteecorn@gmail.com>
2023-10-10 19:19:23 -0300

    Created Agent Model and Migration

--- mvcNestify/mvcNestify/Data/ApplicationDbContext.cs
+++ mvcNestify/mvcNestify/Data/ApplicationDbContext.cs
@@ -1,13 +1,17 @@
 ﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using mvcNestify.Models;
+using System.Security.Cryptography.X509Certificates;
 
 namespace mvcNestify.Data
 {
     public class ApplicationDbContext : IdentityDbContext
     {
+        public DbSet<Agent>? Agents { get; set; }
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
         {
+            
         }
     }
 }
---
+++ mvcNestify/mvcNestify/Data/Migrations/20231010221855_initialDb.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace mvcNestify.Data.Migrations
+{
+    public partial class initialDb : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Agents",
+                columns: table => new
+                {
+                    AgentID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    AgentSIN = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    FirstName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    LastName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    MiddleName = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    DateOfBirth = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    HomePhone = table.Column<string>(type: "nvarchar(12)", maxLength: 12, nullable: true),
+                    CellPhone = table.Column<string>(type: "nvarchar(12)", maxLength: 12, nullable: false),
+                    OfficePhone = table.Column<string>(type: "nvarchar(12)", maxLength: 12, nullable: true),
+                    OfficeEmail = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    StreetAddress = table.Column<string>(type: "nvarchar(90)", maxLength: 90, nullable: false),
+                    Municipality = table.Column<string>(type: "nvarchar(90)", maxLength: 90, nullable: false),
+                    Province = table.Column<string>(type: "nvarchar(3)", maxLength: 3, nullable: false),
+                    PostalCode = table.Column<string>(type: "nvarchar(6)", maxLength: 6, nullable: false),
+                    Username = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Agents", x => x.AgentID);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Agents");
+        }
+    }
+}
