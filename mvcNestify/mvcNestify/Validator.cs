4f9afb863a9d2ebad58eaa08d8ed295d14683162
CarlosLavin1 <carlosfranciscolavin@gmail.com>
2023-10-11 20:27:29 -0300

    cleanup validator class

--- mvcNestify/mvcNestify/Validator.cs
+++ mvcNestify/mvcNestify/Validator.cs
@@ -2,34 +2,6 @@
 
 namespace mvcNestify
 {
-    public class MinAge : ValidationAttribute
-    {
-        private int _Limit;
-        public MinAge(int Limit)
-        { 
-            this._Limit = Limit;
-        }
-        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
-        {
-            DateTime bday = DateTime.Parse(value.ToString());
-            DateTime today = DateTime.Today;
-            int age = today.Year - bday.Year;
-            if (bday > today.AddYears(-age))
-            {
-                age--;
-            }
-            if (age < _Limit)
-            {
-                var result = new ValidationResult("Invalid age, must be 18+");
-                return result;
-            }
-
-
-            return null;
-
-        }
-    }
-
     public static class ValidationHelper
     {
         //helper for DOB validation
