b3be9285df89020bbe20b107a04ba2fb461dda13
Catie <cayteecorn@gmail.com>
2023-10-12 13:08:58 -0300

    Made upload image require user to be logged in

--- mvcNestify/mvcNestify/Controllers/ImageUploadController.cs
+++ mvcNestify/mvcNestify/Controllers/ImageUploadController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 
 namespace mvcNestify.Controllers
 {
@@ -12,6 +13,7 @@ namespace mvcNestify.Controllers
         }
 
         [HttpGet]
+        [Authorize]
         public IActionResult Index()
         {
             return View("Index");
