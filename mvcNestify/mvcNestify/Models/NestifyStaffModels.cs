ec5a8645cabf5cbff7cacbd2afceaa234579acc8
Catie <cayteecorn@gmail.com>
2023-10-12 19:04:15 -0300

    Added all missing Regex, Fixed displays, added verifed agent column

--- mvcNestify/mvcNestify/Models/NestifyStaffModels.cs
+++ mvcNestify/mvcNestify/Models/NestifyStaffModels.cs
@@ -10,6 +10,7 @@ namespace mvcNestify.Models
 
         [Required(ErrorMessage = "Agent's S.I.N number is required")]
         [RegularExpression(@"^\d{3}(-?)\d{3}\1\d{3}$", ErrorMessage = "Please enter a valid S.I.N")]
+        [Display(Name = "Agent S.I.N Number")]
         public string? AgentSIN { get; set; }
 
         [Required(ErrorMessage = "Agent's first name is required")]
@@ -53,19 +54,24 @@ namespace mvcNestify.Models
 
         [Required(ErrorMessage = "A valid street address is required")]
         [Display(Name = "Street Address")]
+        [RegularExpression(@"^[A-Za-z0-9]+(?:\s[A-Za-z0-9'_-]+)+$", ErrorMessage = "Please enter a valid address")]
         [StringLength(90, ErrorMessage = "Street address cannot be longer than 90 characters")]
         public string? StreetAddress { get; set; }
 
         [Required(ErrorMessage = "A municipality name is required")]
+        [RegularExpression(@"^[a-zA-Z]+$", ErrorMessage = "Please enter a valid municipality name")]
         [StringLength(90, ErrorMessage = "Municipality name cannot be longer than 90 characters")]
         public string? Municipality { get; set; }
 
         [Required(ErrorMessage = "Province is required")]
+        [RegularExpression(@"[A-Z]{3}", ErrorMessage = "Please enter a valid province abbreviation")]
         [StringLength(3, ErrorMessage = "Please provide province abbreviation")]
         public string? Province { get; set; }
 
         [Required(ErrorMessage = "Postal Code is required")]
-        [StringLength(6,ErrorMessage ="Postal code cannot be longer than 6 characters")]
+        [RegularExpression(@"^[ABCEGHJKLMNPRSTVXY][0-9][ABCEGHJKLMNPRSTVWXYZ][0-9][ABCEGHJKLMNPRSTVWXYZ][0-9]$", ErrorMessage = "Please enter a valid postal code")]
+        [StringLength(6,ErrorMessage = "Postal code cannot be longer than 6 characters")]
+        [Display(Name = "Postal Code")]
         public string? PostalCode { get; set; }
 
         [Required(ErrorMessage = "Agent username is required")]
@@ -74,6 +80,8 @@ namespace mvcNestify.Models
         public string AuthorizationLevel { get; set; } = "Agent";
 
         public int CreatorID { get; set; } = 0;
+
+        public bool IsVerified { get; set; } = false;
         public DateTime DateOfEmployment { get; set; } = DateTime.Now;
 
     }
