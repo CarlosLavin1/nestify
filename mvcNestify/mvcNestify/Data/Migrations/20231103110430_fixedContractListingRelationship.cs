83a836332284c038514d42408c688a8f90f22d24
Catie <cayteecorn@gmail.com>
2023-11-03 20:27:27 -0300

    Customer select listing display working

---
+++ mvcNestify/mvcNestify/Data/Migrations/20231103110430_fixedContractListingRelationship.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace mvcNestify.Data.Migrations
+{
+    public partial class fixedContractListingRelationship : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ContractID",
+                table: "Listings");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "ContractID",
+                table: "Listings",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+    }
+}
